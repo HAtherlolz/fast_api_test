ea11d4ae3866f10676b7a61ec53512fa91a9b6fe
HAtherlolz <kirill.syusko17@gmail.com>
2022-12-26 16:31:32 +0200

    fixed genre creation

--- src/genre/schema.py
+++ src/genre/schema.py
@@ -3,7 +3,7 @@ from tortoise.contrib.pydantic import pydantic_model_creator
 
 from .models import Genre
 
-Genre_Pydantic = pydantic_model_creator(Genre, name='Genre', exclude=('password',))
+Genre_Pydantic = pydantic_model_creator(Genre, name='Genre', exclude=('password', 'tracks'))
 GenreIn_Pydantic = pydantic_model_creator(Genre, name='GenreIn', exclude_readonly=True)
 
 
